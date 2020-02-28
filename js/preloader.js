39c10f0d033bdb69bae69304aceded4edd01e3e5
lazarus <example@gmail.com>
2020-02-28 13:24:40 +0200

    loader

--- js/preloader.js
+++ js/preloader.js
@@ -3,5 +3,5 @@ document.body.onload = function () {
         var preloader = document.getElementById('page-preloader');
         if (    !preloader.classList.contains('done') ) {
             preloader.classList.add('done');}
-    }, 1000)
+    }, 500)
 }
